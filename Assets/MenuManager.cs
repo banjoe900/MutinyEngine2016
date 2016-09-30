6efe81301f5a739f7fa918643b1997c588247c18
Windows Dropbear <Windows Dropbear>
2016-09-30 19:00:33 +1000

    Added MainMenu Scene, Added GameLevel Scene, Added Placeholder Prefabs, Started Menu Functionality

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+	public GameObject mainMenu;
+	public GameObject teamSelect;
+	public Scene level;
+	bool teamScreen = false;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (teamScreen){
+
+			if (
+
+		}
+
+	}
+
+	void TeamSelect(){
+
+		mainMenu.SetActive = false;
+		teamSelect.SetActive = true;
+
+
+	}
+
+	void MainMenu(){
+
+
+
+	}
+
+
+	void StartGame(){
+
+		SceneManager.LoadScene(level);
+
+	}
+
+
+
+}
