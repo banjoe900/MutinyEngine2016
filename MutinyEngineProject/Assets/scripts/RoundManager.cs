be0c37dae88d4c33f7841383e18fa145b806e580
Jack Chappell <jackchappell1@gmail.com>
2016-09-30 21:09:50 +1000

    put my scripts back in, thanks git

--- MutinyEngineProject/Assets/scripts/RoundManager.cs
+++ MutinyEngineProject/Assets/scripts/RoundManager.cs
@@ -2,9 +2,19 @@
 using System.Collections;
 
 public class RoundManager : MonoBehaviour {
+    private int currentRound;
+    private int blueWins;
+    private int orangeWins;
 
-	// Use this for initialization
-	void Start () {
+    public GameObject player1;
+    public GameObject player2;
+    public GameObject player3;
+    public GameObject player4;
+
+    private UiManager uiManager;
+
+    // Use this for initialization
+    void Start () {
         init();
 	}
 	
@@ -14,7 +24,14 @@ public class RoundManager : MonoBehaviour {
 	}
 
     private void init() {
+        currentRound = 1;
+        blueWins = 0;
+        orangeWins = 0;
 
+        uiManager = GameObject.FindGameObjectWithTag("ui").GetComponent<UiManager>();
+        uiManager.changeRoundNumber(currentRound);
+        uiManager.changeBlueWins(blueWins);
+        uiManager.changeBlueWins(orangeWins);
     }
 
     private void newRound() {
