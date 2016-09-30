f7bfdf05af01b3e02f2a1c9685c51dc70e5017a4
banjoe900 <josephbyrnes@hotmail.com>
2016-09-30 19:54:12 +1000

    Player behavior has sugar level for grannies

---
+++ MutinyEngineProject/Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehavior : MonoBehaviour {
+
+    public float sugarLimit = 100;
+    public float sugarLevel = 0;
+
+    private PlayerMovement playerMovement;
+
+    private bool _isAlive = true;
+    public bool IsAlive
+    {
+        get
+        {
+            return _isAlive;
+        }
+
+        set
+        {
+            _isAlive = value;
+
+            if (_isAlive)
+            {
+
+            }
+            else
+            {
+                Death();
+            }   
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        playerMovement = GetComponent<PlayerMovement>();
+    }	
+
+    public void AddSugar(float damage)
+    {
+        sugarLevel += damage;
+        if(sugarLevel >= sugarLimit)
+        {
+            IsAlive = false;
+        }
+    }
+
+    void Death()
+    {
+        if (playerMovement == null)
+        {
+            playerMovement = GetComponent<PlayerMovement>();
+        }
+        else
+        {
+            playerMovement.isEnabled = false;
+        }
+    }
+}
