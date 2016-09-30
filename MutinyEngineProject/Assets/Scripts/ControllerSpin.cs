9dd2c57b54c3652bd93cf5ca89dda2767c198382
banjoe900 <josephbyrnes@hotmail.com>
2016-10-01 08:25:58 +1000

    A button indicator and controller spin

---
+++ MutinyEngineProject/Assets/Scripts/ControllerSpin.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ControllerSpin : MonoBehaviour {
+
+    private PlayerMovement.ControllerOrientation targetOrientation;
+
+    public Image img;
+    public float speed = 1.2f;
+    private int numberOfRotations = 6;
+    private bool isSpinning = false;
+    private float currentRot = 0;
+
+    private int[] orientationRot = new int[] { 360, 270, 180, 90 };
+	
+	// Update is called once per frame
+	void Update () {
+        float targetRot = (360 * numberOfRotations + orientationRot[(int)targetOrientation]) * -1;
+        currentRot = Mathf.Lerp(currentRot, targetRot, Time.deltaTime * speed);
+        img.rectTransform.localEulerAngles = new Vector3(img.rectTransform.rotation.x, img.rectTransform.rotation.y, currentRot);
+    }
+
+    public void SetOrientation(PlayerMovement.ControllerOrientation orientation)
+    {
+        targetOrientation = orientation;
+        numberOfRotations = Random.Range(7, 14);
+        isSpinning = true;
+        currentRot = 0;
+    }
+}
---
+++ MutinyEngineProject/Assets/Scripts/LookAtCamera.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtCamera : MonoBehaviour {
+
+    public GameObject targetCamera;
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 v = targetCamera.transform.position - transform.position;
+        transform.LookAt(targetCamera.transform.position - v);
+        transform.rotation = (targetCamera.transform.rotation);
+    }
+}
