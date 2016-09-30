cca135e8f378fc91abd90162a1197032feb78557
DylHan <dylan.hanby93@gmail.com>
2016-09-30 20:22:47 +1000

    projectile implementation

---
+++ MutinyEngineProject/Assets/Item_Projectiles.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item_Projectiles : MonoBehaviour
+{
+    public float damage;
+    public float speed;
+    public float lifetime;
+
+    private Rigidbody rb;
+    public GameObject Cookie;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * speed;
+        Destroy(gameObject, lifetime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Obstacle")
+        {
+
+            Destroy(this.gameObject);
+            //   Projectile.projectile = Cookie;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ MutinyEngineProject/Assets/Player_Projectiles.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Projectiles : MonoBehaviour
+{
+    public GameObject projectile;
+    public Transform ProjectileSpawn;
+
+    public float projectileDamage;
+    public float projectileSpeed;
+    public float projectileLifetime;
+    public GameObject Cookie;
+    public GameObject Cake;
+
+    // Use this for initialization
+    void Start()
+    {
+        projectile = Cake;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Instantiate(projectile, ProjectileSpawn.position, ProjectileSpawn.rotation);
+
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            projectile = Cookie;
+        }
+
+    }
+}
