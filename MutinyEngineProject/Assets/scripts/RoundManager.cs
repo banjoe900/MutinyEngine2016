2486b54298c054f7df1496032733257e224732b9
Jack Chappell <jackchappell1@gmail.com>
2016-09-30 18:51:00 +1000

    scene with game gui in it

---
+++ MutinyEngineProject/Assets/scripts/RoundManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoundManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        init();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void init() {
+
+    }
+
+    private void newRound() {
+
+    }
+}
