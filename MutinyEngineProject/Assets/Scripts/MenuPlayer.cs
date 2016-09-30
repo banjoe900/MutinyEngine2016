ab801732b23fc304490264bedae536850bd40c07
Windows Dropbear <Windows Dropbear>
2016-10-01 07:01:56 +1000

    Progress on main menu scene and functionality

--- MutinyEngineProject/Assets/Scripts/MenuPlayer.cs
+++ MutinyEngineProject/Assets/Scripts/MenuPlayer.cs
@@ -3,19 +3,21 @@ using System.Collections;
 
 public class MenuPlayer : MonoBehaviour {
 
+
+
 	public GameObject icon;
 	public GameObject ready;
 
-	private bool playerReady = false;
+	public bool playerReady = false;
 
 	[Range(1, 4)]
-	public int playerNumber = 1;
+	public int playerNumber;
 	public string horizontalAxis;
 	public string playerReadyButton;
 	public float leftThumbstickAngle = 0;
+	int currentState = 0;
 
-
-	public bool isOrange;
+	bool moved;
 
 
 	// Use this for initialization
@@ -26,29 +28,72 @@ public class MenuPlayer : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 
-		if (!playerReady){
-			//Find angle of thumbstick
-			if (Input.GetAxisRaw(horizontalAxis) == 1)
-			{
-				this.gameObject.transform.Translate(new Vector2(90, this.gameObject.transform.position.y));
-				isOrange = false;
-			}
-			else if(Input.GetAxisRaw(horizontalAxis) == -1)
-			{
-				this.gameObject.transform.Translate(new Vector2(-90, this.gameObject.transform.position.y));
-				isOrange = true;
-			}
+		if(Input.GetAxisRaw(horizontalAxis) == 0){
 
-			if(Input.GetButtonDown(playerReadyButton)){
+			moved = false;
 
-				ready.SetActive(true);
-				playerReady = true;
-
-			}
 		}
 
-	
+		if (!playerReady && !moved){
+			//Check which way the player wants to move and see if they meet the requirements
+			if (Input.GetAxisRaw(horizontalAxis) == 1 && currentState != 1)
+			{
+				if(currentState == -1 || MenuManager.instance.tealTeam < 2)
+				{
+					this.gameObject.transform.Translate(new Vector2(350, 0));
+
+					if(transform.localPosition.x > 0){
+						currentState = 1;
+						MenuManager.instance.tealTeam += 1;
+						Debug.Log(MenuManager.instance.orangeTeam);
+						Debug.Log(MenuManager.instance.tealTeam);
+					} else if (currentState == 0 && MenuManager.instance.tealTeam < 2){
+						Debug.Log(MenuManager.instance.orangeTeam);
+						Debug.Log(MenuManager.instance.tealTeam);
+					} else {
+						currentState = 0;
+						MenuManager.instance.orangeTeam -= 1;
+						Debug.Log(MenuManager.instance.orangeTeam);
+						Debug.Log(MenuManager.instance.tealTeam);
+					}
+				}
+
+				moved = !moved;
+
+			}
+			else if(Input.GetAxisRaw(horizontalAxis) == -1 && currentState != -1)
+			{
+				if(currentState == 1 || MenuManager.instance.orangeTeam < 2)
+				{
+					this.gameObject.transform.Translate(new Vector2(-350, 0));
+
+					if(transform.localPosition.x < 0){
+						currentState = -1;
+						MenuManager.instance.orangeTeam += 1;
+					} else if (currentState == 0 && MenuManager.instance.orangeTeam < 2){
+						//don't minus stuff
+					} else {
+						currentState = 0;
+						MenuManager.instance.tealTeam -= 1;
+
+					moved = !moved;
+					}
+				}
+			
+			}
+
+			if(Input.GetButtonDown(playerReadyButton))
+			{
+				Debug.Log(playerReadyButton);
+				if(currentState != 0){
+					ready = Instantiate(ready, this.transform.position, Quaternion.identity) as GameObject;
+					ready.transform.SetParent(this.transform);
+					playerReady = true;
+				}
+			}
+		}
 	}
 }
