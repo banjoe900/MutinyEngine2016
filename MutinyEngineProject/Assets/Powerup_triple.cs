b4b860428159349df8211ca53d2927277ac2ab21
DylHan <dylan.hanby93@gmail.com>
2016-10-01 11:29:30 +1000

    no message

---
+++ MutinyEngineProject/Assets/Powerup_triple.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Powerup_triple : MonoBehaviour {
+    public float Powerup_time;
+    // Use this for initialization
+    void Start() {
+
+        
+
+
+    }
+
+
+
+// Update is called once per frame
+void Update () {
+	
+	}
+}
